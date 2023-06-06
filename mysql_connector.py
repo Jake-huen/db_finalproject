2ac0f8a174928dc7f20f192c386aa7d8178c748c
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-06-07 01:06:09 +0900

    id들 AUTO_INCREMENT 설정

--- mysql_connector.py
+++ mysql_connector.py
@@ -17,7 +17,7 @@ table_queries = [
     """,
     """
     CREATE TABLE movie (
-      id INT PRIMARY KEY,
+      id INT PRIMARY KEY AUTO_INCREMENT,
       movie_name_ko VARCHAR(255),
       movie_name_en VARCHAR(255),
       release_year VARCHAR(50),
@@ -32,7 +32,7 @@ table_queries = [
     """,
     """
     CREATE TABLE director (
-      id INT PRIMARY KEY,
+      id INT PRIMARY KEY AUTO_INCREMENT,
       name VARCHAR(255)
     )
     """,
@@ -41,7 +41,7 @@ table_queries = [
     """,
     """
     CREATE TABLE genre (
-      id INT PRIMARY KEY,
+      id INT PRIMARY KEY AUTO_INCREMENT,
       name VARCHAR(255)
     )
     """
@@ -74,17 +74,17 @@ create_tables()
 def db_insert():
     df = pd.read_excel("movie_data.xls")
     df = df.where(pd.notnull(df), None)
-    sql_movie_insert = "insert into movie (id, movie_name_ko, movie_name_en, release_year, release_country, type, release_state, release_company) values (%s, %s, %s, %s, %s, %s, %s, %s)"
-    sql_director_insert = "insert into director (id, name) values (%s, %s)"
-    sql_genre_insert = "insert into genre (id, name) values (%s, %s)"
+    sql_movie_insert = "insert into movie (movie_name_ko, movie_name_en, release_year, release_country, type, release_state, release_company) values (%s, %s, %s, %s, %s, %s, %s)"
+    sql_director_insert = "insert into director (name) values (%s)"
+    sql_genre_insert = "insert into genre (name) values (%s)"
     movie_list = []
     director_list = []
     genre_list = []
     for index, row in df.iloc[4:100].iterrows():
         movie_list.append(
-            (index - 3, row[0], row[1], row[2], row[3], row[4], row[6], row[8]))  # row[5] : 장르, row[7]: 감독
-        genre_list.append((index - 3, row[5]))
-        director_list.append((index - 3, row[7]))
+            (row[0], row[1], row[2], row[3], row[4], row[6], row[8]))  # row[5] : 장르, row[7]: 감독
+        genre_list.append((row[5]))
+        director_list.append((row[7]))
     curs.executemany(sql_movie_insert, movie_list)
     curs.executemany(sql_director_insert, director_list)
     curs.executemany(sql_genre_insert, genre_list)
