df95c4e5fdccb104aa7cfe30d9b96799ebec4b56
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2023-06-07 01:03:05 +0900

    db에 movie, director, genre 삽입(연관관계 매핑x)

--- mysql_connector.py
+++ mysql_connector.py
@@ -1,32 +1,96 @@
 import pandas as pd
 import pymysql
+
+
 def db_connect():
     con = pymysql.connect(host='localhost', user='taeheon',
-                          password='rlaxogjs8312', db='movie', charset='utf8')
+                          password='rlaxogjs8312', db='movie_db', charset='utf8')
     cur = con.cursor(pymysql.cursors.DictCursor)
     return con, cur
 
 
 conn, curs = db_connect()
 
+table_queries = [
+    """
+    DROP TABLE IF EXISTS movie;
+    """,
+    """
+    CREATE TABLE movie (
+      id INT PRIMARY KEY,
+      movie_name_ko VARCHAR(255),
+      movie_name_en VARCHAR(255),
+      release_year VARCHAR(50),
+      release_country VARCHAR(50),
+      type VARCHAR(50),
+      release_state VARCHAR(50),
+      release_company VARCHAR(50)
+    )
+    """,
+    """
+    DROP TABLE IF EXISTS director;
+    """,
+    """
+    CREATE TABLE director (
+      id INT PRIMARY KEY,
+      name VARCHAR(255)
+    )
+    """,
+    """
+    DROP TABLE IF EXISTS genre;
+    """,
+    """
+    CREATE TABLE genre (
+      id INT PRIMARY KEY,
+      name VARCHAR(255)
+    )
+    """
+    # """
+    # DROP TABLE IF EXISTS movie_director;
+    # """,
+    # """
+    # CREATE TABLE movie_director (
+    #   movie_id INT,
+    #   director_id INT,
+    #   PRIMARY KEY (movie_id, director_id),
+    #   FOREIGN KEY (movie_id) REFERENCES movie(id),
+    #   FOREIGN KEY (director_id) REFERENCES director(id)
+    # )
+    # """
+]
+
+
+# 테이블 생성 함수
+def create_tables():
+    for query in table_queries:
+        curs.execute(query)
+    conn.commit()
+    print("테이블이 잘 생성되었습니다.")
+
+
+create_tables()
+
 
 def db_insert():
-    sql_insert = "insert into student (sno, sname, dept) values (%s, %s, %s)"
-    # a = (7000, '김선경', '컴퓨터')
-    # b = (8000, '황산성', '산업공학')
-    # c = (9000, '김호일', '경영학')
-    # student_list = [a, b, c]
-
-    # curs.executemany(sql_insert, student_list)
-    # conn.commit()
-
-
-def get_movie_db_excel():
     df = pd.read_excel("movie_data.xls")
-    print(df)
+    df = df.where(pd.notnull(df), None)
+    sql_movie_insert = "insert into movie (id, movie_name_ko, movie_name_en, release_year, release_country, type, release_state, release_company) values (%s, %s, %s, %s, %s, %s, %s, %s)"
+    sql_director_insert = "insert into director (id, name) values (%s, %s)"
+    sql_genre_insert = "insert into genre (id, name) values (%s, %s)"
+    movie_list = []
+    director_list = []
+    genre_list = []
+    for index, row in df.iloc[4:100].iterrows():
+        movie_list.append(
+            (index - 3, row[0], row[1], row[2], row[3], row[4], row[6], row[8]))  # row[5] : 장르, row[7]: 감독
+        genre_list.append((index - 3, row[5]))
+        director_list.append((index - 3, row[7]))
+    curs.executemany(sql_movie_insert, movie_list)
+    curs.executemany(sql_director_insert, director_list)
+    curs.executemany(sql_genre_insert, genre_list)
+    conn.commit()
 
-
-get_movie_db_excel()
+db_insert()
 
 
 def db_delete():
